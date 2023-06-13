d8398a5dad4306448cc0e60ec3e8545b79ca1c5d
Ali Bayat <ali.bayat77@yahoo.com>
2023-06-14 01:13:40 +0430

    style binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -12,14 +12,20 @@ export class TestComponent {
   public secondInputValue: string = 'second Input';
   public isDisabled = false;
 
-  public hasError = false;
+  public hasError = true;
   public isSpecial = true;
-  
+
   public messageClasses = {
     'text-red': this.hasError,
     'text-blue': !this.hasError,
-    'text-special': this.isSpecial
-  }
+    'text-special': this.isSpecial,
+  };
+
+  public orange = 'orange';
+  public styleBindingObject = {
+    color: 'purple',
+    fontStyle: 'italic',
+  };
 
   siteUrl: string = window.location.href;
 }
