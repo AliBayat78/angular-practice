3bf853cc431e39c77d0b846c81b4dabf5cc726bf
Ali Bayat <ali.bayat77@yahoo.com>
2023-06-13 20:43:39 +0430

    #bind property

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent {
+  name: string = 'ali';
 
+  public firstInputValue: string = 'first Input';
+  public secondInputValue: string = 'second Input';
+  public isDisabled = false;
+
+  siteUrl: string = window.location.href;
 }
