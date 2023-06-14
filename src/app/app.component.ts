691fdb8994166161ecdf67baf5ad39095ee31dbc
Ali Bayat <ali.bayat77@yahoo.com>
2023-06-14 20:37:31 +0430

    component interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-practice';
+
+  public outPostedMessage: string = '';
+  public receivedMessage: string = '';
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
   public names = ['ali', 'ahmad', 'reza', 'mamad'];
+  public inputValue = 'ss';
 
+  inputValueHandler(name: string) {
+    this.inputValue = name;
+  }
+
+  public childMessage: string = '';
+  @Output() public childEvent = new EventEmitter();
+  childMessageHandler() {
+    this.childEvent.emit(this.childMessage);
+  }
+
+  @Input('parentData') public messageFromParent: string = '';
 }
