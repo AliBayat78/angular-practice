ae9fd22c5c7bf9411969f1824429849f21ab5558
Ali Bayat <ali.bayat77@yahoo.com>
2023-06-14 19:26:00 +0430

    *ngFor Directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,36 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.scss'],
 })
 export class TestComponent {
-  name: string = 'ali';
+  public names = ['ali', 'ahmad', 'reza', 'mamad'];
 
-  public firstInputValue: string = 'first Input';
-  public secondInputValue: string = 'second Input';
-  public isDisabled = false;
-
-  public hasError = true;
-  public isSpecial = true;
-
-  public messageClasses = {
-    'text-red': this.hasError,
-    'text-blue': !this.hasError,
-    'text-special': this.isSpecial,
-  };
-
-  public orange = 'orange';
-  public styleBindingObject = {
-    color: 'purple',
-    fontStyle: 'italic',
-  };
-
-  clickHandler(event: MouseEvent) {
-    this.name = 'meow';
-  }
-
-  logMessage(ref: string) {
-    console.log(ref);
-  }
-
-  public color: string = 'black';
-
-  siteUrl: string = window.location.href;
 }
