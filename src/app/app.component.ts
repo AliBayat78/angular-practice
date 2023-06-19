be3534eb688f034eab36017a3a571611f1b436d1
Ali Bayat <ali.bayat77@yahoo.com>
2023-06-20 03:36:51 +0430

    @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ComponentRef,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'Angular-practice';
 
-  public inputValue: string = 'default';
+  private _inputValue: string = 'default';
 
-  alertHandler(updatedValue: string) {
-    this.inputValue = updatedValue;
-    if (updatedValue === 'alert') {
-      alert('this is an alert');
+  get inputValue(): string {
+    return this._inputValue;
+  }
+
+  set inputValue(value: string) {
+    this._inputValue = value;
+    if (value === 'alert') {
+      alert('alert');
     }
+  }
+
+  @ViewChild('inputReference', { static: false })
+  inputReference: ElementRef<HTMLInputElement> = {} as ElementRef;
+
+  @ViewChild('childComponent', { static: false })
+  childReference!: ElementRef<TestComponent>;
+
+  ngAfterViewInit(): void {
+    console.log(this.inputReference.nativeElement);
+
+    if (this.childReference) {
+      console.log(this.childReference.nativeElement.employees);
+    }
+  }
+
+  ngOnInit() {
+    console.log(this.inputReference);
   }
 
   constructor(private router: Router) {}
